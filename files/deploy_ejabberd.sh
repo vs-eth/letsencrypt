dba050c891a79f6ecbf4e875a35844ee7df8b4bc
Maximilian Falkenstein <falkensm@student.ethz.ch>
2017-02-23 00:10:24 +0100

    Fix deploy script, allow for an extra hostname (hacky) and fix changed issue when using webroot

--- files/deploy_ejabberd.sh
+++ files/deploy_ejabberd.sh
@@ -18,9 +18,18 @@ ejabberd_deploy() {
 
   _cwd="${0%/*}/${_cdomain}"
 
+  # Ejabberds special magic file
   cat $_ckey $_ccert $_cca > $_target
   chown ejabberd:ejabberd $_target
   chmod 640 $_target
 
   systemctl restart ejabberd
+  
+  # We also have nginx, mainly for serving the verification as standalone mode makes it impossible to use sshfs
+  cp $_ckey /etc/nginx/certs/sosxmpp.ethz.ch/privkey.pem
+  cat $_ccert $_cca > /etc/nginx/certs/sosxmpp.ethz.ch/fullchain.pem
+  chown -R root:nginx /etc/nginx/certs/sosxmpp.ethz.ch
+  chmod -R 640 /etc/nginx/certs/sosxmpp.ethz.ch
+  
+  systemctl restart nginx
 }
